5f3cea9535094acca8c226ecbd88913993720471
Jeroen Janssens <jeroen@jeroenjanssens.com>
2017-07-10 17:30:00 +0100

    Rename test-zzz.R to test-server.R because each test context is now idempotent anyway

--- tests/testthat/test-server.R
+++ tests/testthat/test-server.R
@@ -3,7 +3,7 @@ context("server")
 if (is_running()) stop("server is already running")
 
 test_that("the server can be killed", {
-  new_session()
+  new_session()  # This is the only way to start the server
   expect_error(kill_server(), NA)
   expect_error(kill_server())
 })
