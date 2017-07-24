4761bbc548657d7423bdd9b35261c437c8fc2e73
Jeroen Janssens <jeroen@jeroenjanssens.com>
2017-07-24 21:01:57 +0200

    Move pipe_pane function to pane.R

--- R/pane.R
+++ R/pane.R
@@ -16,6 +16,26 @@ list_panes <- function(target = NULL) {
 }
 
 #' @export
+pipe_pane <- function(target = NULL, shell_command = NULL, open = FALSE) {
+  args <- c()
+
+  if (!is.null(target)) {
+    args <- c(args, "-t", target$name)
+  }
+
+  if (open) {
+    args <- c(args, "-o")
+  }
+
+  if (!is.null(shell_command)) {
+    args <- c(args, shQuote(shell_command))
+  }
+  tmux_pipe_pane(args)
+  invisible(target)
+}
+
+
+#' @export
 print.tmuxr_pane <- function(x, ...) {
   lines <- tmux_list_panes("-a")
   status <- lines[grepl(stringr::str_interp("^${x$name}:.*$"), lines)]
