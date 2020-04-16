8724de450bffb4360404ac2e97ea86c6092de5a6
Jeroen Janssens <jeroen@jeroenjanssens.com>
2020-04-16 22:46:05 +0200

    Add option for specifying config file

--- R/tmux.R
+++ R/tmux.R
@@ -2,8 +2,12 @@
 tmux_command <- function(command, ...) {
   stopifnot(rlang::is_scalar_character(command))
 
+  tmux_args <- c(
+    "-f", getOption("tmux_config_file", "/dev/null")
+    )
+
   suppressWarnings(
-    result <- system2("tmux", c(command, ...), stdout = TRUE, stderr = TRUE)
+    result <- system2("tmux", c(tmux_args, command, ...), stdout = TRUE, stderr = TRUE)
   )
 
   status <- attr(result, "status")
