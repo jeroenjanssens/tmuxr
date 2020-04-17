63735485e40296017926358b6a5c4f60a630d2c2
Jeroen Janssens <jeroen@jeroenjanssens.com>
2020-04-17 22:27:45 +0200

    Change capture test slightly

--- tests/testthat/test-pane.R
+++ tests/testthat/test-pane.R
@@ -1,7 +1,7 @@
 context("pane")
 
 test_that("contents can be captured", {
-  s <- new_session(shell_command = "PS1='$ ' bash", height = 10)
+  s <- new_session(shell_command = "bash -c 'PS1=\"$ \" bash'", height = 10)
   expect_length(capture_pane(s), 10)
   expect_match("$\n\n\n\n\n\n\n\n\n", capture_pane(s, cat = TRUE), fixed = TRUE)
 })
