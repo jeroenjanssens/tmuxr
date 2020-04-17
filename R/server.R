5f76ab7649859098d1af0919a65464d627d747b6
Jeroen Janssens <jeroen@jeroenjanssens.com>
2020-04-17 14:59:31 +0200

    Simplify is_running function

--- R/server.R
+++ R/server.R
@@ -18,23 +18,7 @@ start_server <- function() {
 #'
 #' @export
 is_running <- function() {
-
-  tmux_args <- c()
-
-  if (!is.null(getOption("tmux_config_file")))
-    tmux_args <- c(tmux_args, "-f", getOption("tmux_config_file"))
-
-  if (!is.null(getOption("tmux_socket_name")))
-    tmux_args <- c(tmux_args, "-L", getOption("tmux_socket_name"))
-
-  if (!is.null(getOption("tmux_socket_path")))
-    tmux_args <- c(tmux_args, "-S", getOption("tmux_socket_path"))
-
-  result <- suppressWarnings(
-    system2("tmux", tmux_args, "list-sessions", stdout = TRUE, stderr = TRUE)
-  )
-  status <- attr(result, "status")
-  (is.null(status) || (status == 0))
+  tryCatch({tmuxr::list_sessions(); TRUE}, error = function(cond) {FALSE})
 }
 
 
