fa1e48477d5d45beba895db842a2a0b9094063da
Jeroen Janssens <jeroen@jeroenjanssens.com>
2020-04-18 13:55:46 +0200

    Make capture-pane test work for version < 2.6

--- tests/testthat/test-pane.R
+++ tests/testthat/test-pane.R
@@ -1,9 +1,26 @@
 context("pane")
 
 test_that("contents can be captured", {
-  s <- new_session(shell_command = "bash -c 'PS1=\"$ \" bash'", height = 10)
-  expect_length(capture_pane(s), 10)
-  expect_match("$\n\n\n\n\n\n\n\n\n", capture_pane(s, cat = TRUE), fixed = TRUE)
+
+  specified_height <- 10
+  s <- new_session(shell_command = "cat", height = specified_height)
+
+  # Prior to version 2.6, the actual height was one less than the specified height.
+  # See https://github.com/tmux/tmux/blob/349617a818ec8ed0f1cdedac64f5d9126d920f87/CHANGES#L634-L636
+
+  if (tmux_version(as_numeric = TRUE) >= 2.6) {
+    expected_height <- specified_height
+  } else {
+    expected_height <- specified_height - 1
+  }
+
+  expect_length(capture_pane(s), expected_height)
+  expect_match(paste0(rep("\n", expected_height), collapse = ""),
+               capture_pane(s, cat = TRUE), fixed = TRUE)
+
+  expect_length(capture_pane(s, start = 0, end = 1), 2)
+
+  kill_session(s)
 })
 
 kill_server()
