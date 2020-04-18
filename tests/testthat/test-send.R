610d3cb6cb40c1962016a0e3d8f5e10b21d07a97
Jeroen Janssens <jeroen@jeroenjanssens.com>
2020-04-18 13:56:00 +0200

    Create tests for send_keys

---
+++ tests/testthat/test-send.R
@@ -0,0 +1,58 @@
+context("send")
+
+test_that("keys can be sent", {
+  cp <- function(x) capture_pane(x, start = 0, end = 5)
+
+  s <- new_session(shell_command = "cat")
+  expect_identical(cp(s), c("",
+                            "",
+                            "",
+                            "",
+                            "",
+                            ""))
+
+  send_keys(s, "hello")
+  expect_identical(cp(s), c("hello",
+                            "",
+                            "",
+                            "",
+                            "",
+                            ""))
+
+  send_keys(s, "Space world? BSpace !")
+  expect_identical(cp(s), c("hello world!",
+                            "",
+                            "",
+                            "",
+                            "",
+                            ""))
+
+  send_keys(s, "Enter")
+  expect_identical(cp(s), c("hello world!",
+                            "hello world!",
+                            "",
+                            "",
+                            "",
+                            ""))
+
+  send_keys(s, "Speak Friend and Enter")
+  expect_identical(cp(s), c("hello world!",
+                            "hello world!",
+                            "SpeakFriendand",
+                            "SpeakFriendand",
+                            "",
+                            ""))
+
+  send_keys(s, "Speak Friend and Enter", literal = TRUE)
+  expect_identical(cp(s), c("hello world!",
+                            "hello world!",
+                            "SpeakFriendand",
+                            "SpeakFriendand",
+                            "Speak Friend and Enter",
+                            ""))
+
+  send_keys(s, "C-c") # exits cat and closes the session
+  expect_error(cp(s))
+})
+
+kill_server()
