3ea2ad418efda75f5807ce4f2ace8e380c056441
Jeroen Janssens <jeroen@jeroenjanssens.com>
2020-04-25 19:50:59 +0200

    Remove because OSX on Travis complains about long filenames

--- tests/testthat/setup-tmuxr.R
+++ tests/testthat/setup-tmuxr.R
@@ -1,6 +1,6 @@
-options(tmux_config_file = "/dev/null",
-        tmux_socket_name = "test",
-        tmux_socket_path = tempfile("tmuxr-socket-"))
+# options(tmux_config_file = "/dev/null",
+#         tmux_socket_name = "test",
+#         tmux_socket_path = tempfile("tmuxr-socket-"))
 
 if (!is_installed()) stop("tmux not found")
 if (is_running()) stop("server is already running")
