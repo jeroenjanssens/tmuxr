3c805f35b6d6640035c695b1f754d71a241f9ee6
Jeroen Janssens <jeroen@jeroenjanssens.com>
2020-04-26 14:12:45 +0200

    Fix install script

--- .travis-tmux.sh
+++ .travis-tmux.sh
@@ -2,8 +2,8 @@
 if [ $TRAVIS_OS_NAME = linux ]
 then
     echo "Installing tmux ${TMUX_VERSION}..."
-    wget https://github.com/tmux/tmux/releases/download/3.1/tmux-${TMUX_VERSION}.tar.gz
+    wget https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
     tar -xzf tmux-${TMUX_VERSION}.tar.gz
     cd tmux-${TMUX_VERSION}
-    ./configure && make && sudo make install
+    ./configure && make && sudo make install && echo "Successfully installed tmux ${TMUX_VERSION}"
 fi
