504f908cf3768def68aa044d7971f9d52aeaad89
Jeroen Janssens <jeroen@jeroenjanssens.com>
2020-04-26 14:25:25 +0200

    Install tmux from tmp dir

--- .travis-tmux.sh
+++ .travis-tmux.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+set -xe
 if [ $TRAVIS_OS_NAME = linux ]
 then
     echo "Installing tmux ${TMUX_VERSION}..."
+    cd /tmp
     wget https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
     tar -xzf tmux-${TMUX_VERSION}.tar.gz
     cd tmux-${TMUX_VERSION}
-    ./configure && make && sudo make install && echo "Successfully installed tmux ${TMUX_VERSION}"
+    ./configure
+    make
+    sudo make install
+    echo "Successfully installed tmux ${TMUX_VERSION}"
 fi
