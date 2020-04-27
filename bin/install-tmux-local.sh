343ad46ea7ed34062e8674d64d9295ebee8ff59d
Jeroen Janssens <jeroen@jeroenjanssens.com>
2020-04-27 13:37:57 +0200

    Move travis install script and add script to install different versions of tmux locally

---
+++ bin/install-tmux-local.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -xe
+TMUX_VERSION=${1:-"3.1"}
+echo "Installing tmux ${TMUX_VERSION}..."
+cd /tmp
+wget https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
+tar -xzf tmux-${TMUX_VERSION}.tar.gz
+cd tmux-${TMUX_VERSION}
+./configure --prefix=$HOME --program-suffix=-${TMUX_VERSION}
+make install-binPROGRAMS
+echo "Successfully installed tmux ${TMUX_VERSION} in ${HOME}/bin"
