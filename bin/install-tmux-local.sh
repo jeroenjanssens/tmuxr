4200fddf692c1e6d5847dde29cd97ea7f222de47
Jeroen Janssens <jeroen@jeroenjanssens.com>
2024-04-25 13:23:24 +0200

    Fix install script

--- bin/install-tmux-local.sh
+++ bin/install-tmux-local.sh
@@ -6,6 +6,6 @@ cd /tmp
 wget https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
 tar -xzf tmux-${TMUX_VERSION}.tar.gz
 cd tmux-${TMUX_VERSION}
-./configure --prefix=$HOME --program-suffix=-${TMUX_VERSION}
+./configure --prefix=$HOME --program-suffix=-${TMUX_VERSION} --disable-utf8proc
 make install-binPROGRAMS
 echo "Successfully installed tmux ${TMUX_VERSION} in ${HOME}/bin"
